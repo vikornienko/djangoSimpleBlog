e0a78a903282fc55e7062c4b80ea0f812dee70c6
vikornienko <vikornienko76@gmail.com>
2023-04-22 12:49:37 +0500

    Sixth commit Added function get_absolute_url in Post model.

--- appblog/urls.py
+++ appblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import post_list
+from .views import post_list, post_detail
 
 app_name = "appblog"
 
 urlpatterns = [
-    path('', post_list)
+    path("", post_list, name="post_list"),
+    path("<int:id>/", post_detail, name="post_detail"),
 ]
--- appblog/views.py
+++ appblog/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
 
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "appblog/post/list.html", {"posts": posts})
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, "appblog/post/detail.html", {"post": post})
+
