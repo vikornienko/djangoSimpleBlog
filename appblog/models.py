7b2a593d008a2595333fc99cc4bafdb21fa9ea01
vikornienko <vikornienko76@gmail.com>
2023-04-22 17:57:23 +0500

    The seventh commit Added pagination and exception handling for invalid page number.

--- appblog/models.py
+++ appblog/models.py
@@ -43,4 +43,8 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail',
+                       args=[self.publish_at.year,
+                             self.publish_at.month,
+                             self.publish_at.day,
+                             self.slug])
--- appblog/views.py
+++ appblog/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, "appblog/post/list.html", {"posts": posts})
 
-def post_detail(request, id):
+def post_detail(request, post, year, month, day):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post found.")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish_at__year=year,
+                             publish_at__month=month,
+                             publish_at__day=day)
 
     return render(request, "appblog/post/detail.html", {"post": post})
 
