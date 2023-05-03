a052c000d143348384302d34ff13a610c113e8bf
vikornienko <vikornienko76@gmail.com>
2023-05-03 21:29:31 +0500

    The ninth commit Added selenium package for integration testing.  Added blog homepage test.

---
+++ test_integration.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from django.test import LiveServerTestCase
+
+class Hosttest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = WebDriver("./other_files/cdriver/chromedriver/chromedriver")
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def test_home_page(self):
+        self.driver.get(f"{self.live_server_url}/blog/")
+        self.driver.save_screenshot("./other_files/screenshots/image.jpg")
+        assert "My Blog" in self.driver.title
